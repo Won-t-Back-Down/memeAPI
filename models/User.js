f7f22a6764c020ea7fb763e3234001c14681c57e
bryan99pham <bryan99pham@gmail.com>
2022-11-17 12:37:19 -0600

    formatted Meme model

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+const { database } = require("../db");
+
+const Meme = database.define("meme", {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    caption: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    tags: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    image: {
+      type: Sequelize.STRING,
+      allowNull: false
+    }
+});
+
+module.exports = {
+    Pokemon
+}

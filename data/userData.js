bd90b217b594f931687c1f87bc7c04a43139f567
bryan99pham <bryan99pham@gmail.com>
2022-11-17 13:33:16 -0600

    added seeding function to db

---
+++ data/userData.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+      id: 1, 
+      first_name: "Bobby", 
+      last_name: "Bob",
+  },
+  {
+      id: 2,
+      first_name: "Billy",
+      last_name: "Bill"
+  }
+];
+
+module.exports = {
+  users
+}

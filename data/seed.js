97662d8cd9c3b1573d84a082763fe612f4a4c1b6
bryan99pham <bryan99pham@gmail.com>
2023-01-05 13:47:57 -0600

    added hashing of passwords for seeded users

--- data/seed.js
+++ data/seed.js
@@ -2,14 +2,27 @@ const {database} = require('../db');
 const {User, Meme} = require('../models/index');
 const {memes} = require("./memeData");
 const {users} = require("./userData");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+
+const SALT_COUNT = 9;
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 //File for if you want to use data you already made up
 let seed = async () => {
     await database.sync({force:true});
 
     // Create the entries for them in their Models
+    for (let user of users) {
+        const { first_name, last_name, username, password, isAdmin } = user;
+        const hashedPw = await bcrypt.hash(password, SALT_COUNT);
+        const newUser = await User.create({ first_name, last_name, username, password: hashedPw, isAdmin });
+        const token = jwt.sign({ id: newUser.id, username }, process.env.JWT_SECRET);
+    }
+
     let memeEntries = await Meme.bulkCreate(memes);
-    let userEntries = await User.bulkCreate(users);
 
     // let firstMeme = await memeEntries[0];
     // let secondUser = await userEntries[1];

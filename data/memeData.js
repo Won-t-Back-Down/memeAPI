0c486810082061ddc9debd443e00122988d676d0
Igmer Castillo <icastillo@box.com>
2023-01-10 13:35:03 -0800

    added Auth

--- data/memeData.js
+++ data/memeData.js
@@ -4,7 +4,7 @@ const memes = [
       caption: "Troll Face",
       tags: "classic",
       image: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Trollface_non-free.png/220px-Trollface_non-free.png",
-      // userId: 1
+    //  userId: 1
   },
   {
       id: 2,
--- data/seed.js
+++ data/seed.js
@@ -1,7 +1,7 @@
-const {database} = require('../db');
-const {User, Meme} = require('../models/index');
-const {memes} = require("./memeData");
-const {users} = require("./userData");
+const { database } = require("../db");
+const { User, Meme } = require("../models/index");
+const { memes } = require("./memeData");
+const { users } = require("./userData");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 require("dotenv").config();
@@ -12,27 +12,34 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 //File for if you want to use data you already made up
 let seed = async () => {
-    await database.sync({force:true});
+  await database.sync({ force: true });
 
-    let memeEntries = await Meme.bulkCreate(memes);
+  let memeEntries = await Meme.bulkCreate(memes);
 
-    // Create the entries for them in their Models
-    for (let user of users) {
-        const { first_name, last_name, username, password, isAdmin } = user;
-        const hashedPw = await bcrypt.hash(password, SALT_COUNT);
-        const newUser = await User.create({ first_name, last_name, username, password: hashedPw, isAdmin });
-        const token = jwt.sign({ id: newUser.id, username }, process.env.JWT_SECRET);
-    }
+  // Create the entries for them in their Models
+  for (let user of users) {
+    const { first_name, last_name, username, password, isAdmin } = user;
+    const hashedPw = await bcrypt.hash(password, SALT_COUNT);
+    const newUser = await User.create({
+      first_name,
+      last_name,
+      username,
+      password: hashedPw,
+      isAdmin,
+    });
+    // const token = jwt.sign({ id: newUser.id, username }, process.env.JWT_SECRET);
+  }
 
-    // let firstMeme = await memeEntries[0];
-    // let secondUser = await userEntries[1];
+  let firstMeme = await Meme.findByPk(1);
+  let bobby = await User.findByPk(1);
 
-    // console.log("Test 1: ", firstMeme);
-    // console.log("Test 2: ", secondUser);
+  //  await firstMeme.setUser(bobby)
+  await bobby.addMeme(firstMeme);
+  // console.log("Test 1: ", firstMeme);
+  // console.log("Test 2: ", secondUser);
 
-    // console.log("The database is populated... Test it out using a Local Server!")
-}
+  // console.log("The database is populated... Test it out using a Local Server!")
+};
 
-
-seed()
+seed();
 //node data/seed
